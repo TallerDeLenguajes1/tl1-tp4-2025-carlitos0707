cbc87f7dd873ea155275f084d4d066b2b9977507
CARLOS URIBURU <carlosuriburupadilla@gmail.com>
2025-04-15 13:39:01 -0300

    interfaz de usuario

--- distribuidora.c
+++ distribuidora.c
@@ -22,16 +22,59 @@ typedef Nodo* Lista;
 Lista crearLista();
 Lista cargaTareas(Lista L);
 int esListaVacia(Lista L);
+int tamanio(Lista L);
 void mostrar(Lista L);
+Tarea buscarPorID(Lista L, short int id);
+Tarea buscarPorPalabra(Lista L);
 Lista cargarRealizadas(Lista L1,Lista L2);
 
 
 int main(){
+    int op,t,busqueda;
+    char palabra[30];
+    Tarea tarea;
     srand(time(NULL));
-    Lista TareasPendientes;
+    Lista TareasPendientes,TareasRealizadas;
     TareasPendientes = crearLista();
     TareasPendientes = cargaTareas(TareasPendientes);
+    TareasRealizadas = crearLista();
+    TareasRealizadas = cargarRealizadas(TareasPendientes,TareasRealizadas);
     mostrar(TareasPendientes);
+    mostrar(TareasRealizadas);
+    do
+    {
+        printf("\n\n\nSeleccione una opcion\n\n\n");
+        printf("\t 0. Tareas pendientes\n");
+        printf("\t 1. Tareas realizadas\n");
+        scanf("%d",&t);
+        printf("\n\nSeleccione una operacion\n\n");
+        printf("\t 0. busqueda por ID\n");
+        printf("\t 1. busqueda por palabra");
+        scanf("%d",busqueda);
+        if (t == 0)
+        {
+            if (busqueda == 0)
+            {
+                tarea = buscarPorID(TareasPendientes);
+            }
+            else
+            {
+                tarea = buscarPorPalabra(TareasPendientes);
+            }
+        }
+        else{
+            if (busqueda == 0)
+            {
+                tarea = buscarPorID(TareasRealizadas);
+            }
+            else
+            {
+                tarea = buscarPorPalabra(TareasRealizadas);
+            }
+        }
+        
+    } while (op == 0);
+    
 }
 
 
@@ -97,4 +140,62 @@ void mostrar(Lista L){
             aux = aux->Siguiente;
         }
     }
+}
+
+int tamanio(Lista L){
+    Lista aux = L;
+    int cont=0;
+    while (esListaVacia(aux) == 1)
+    {
+        cont++;
+        aux = aux->Siguiente;
+    }
+    return cont;
+}
+
+
+
+Lista cargarRealizadas(Lista L1,Lista L2){
+    int op,id;
+    Tarea tarea;
+    printf("\n\n\nLista de tareas pendientes\n\n\n");
+    mostrar(L1);
+    do
+    {      
+        printf("Cargar otra tarea\n\n");
+        printf("\t 0. SI\n");
+        printf("\t 1. NO\n");
+        scanf("%d",&op);
+    } while (op == 0);
+    
+}
+
+
+Tarea buscarPorID(Lista L){
+    Lista aux = L;
+    int id;
+    do
+        {
+            printf("\n\nSeleccione el ID de la tarea completada\n\n");
+            scanf("%d",&id);
+            if (id > tamanio(L))
+            {
+                printf("No se pudo encontrar la tarea\n");
+            }
+            if (id < 0)
+            {
+                printf("ID Invalido \n");
+            }                       
+        } while (id < 0 || id > tamanio(L));
+    for (int i = 0; i < id; i++)
+    {
+        aux = aux->Siguiente;
+    }
+    return aux->T;
+}
+
+
+Tarea buscarPorPalabra(Lista L){
+    Lista aux;
+    printf("Ingrese una palabra\n");
 }
